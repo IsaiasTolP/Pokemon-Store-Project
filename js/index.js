04cb42f9520548424c5d6525e34c50e0bc96adf3
IsaiasTolP <toledop.isaias@gmail.com>
2024-09-23 15:35:30 +0100

    Javascript

--- js/index.js
+++ js/index.js
@@ -2,7 +2,13 @@ const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
     // window.alert("Hola");
-    document.querySelectorAll(`#filtro`).forEach( (e) => {
-        e.style.visibility = "visible";
+    document.querySelectorAll("#filtro").forEach(element => {
+        element.style.visibility = "visible";
     });
+
+    document.querySelectorAll("#btn_lista_deseo").forEach(element => {
+        element.style.visibility = "visible";
+    });
+
+    document.querySelector(".listaPokemon").style.visibility = "visible";
 });
