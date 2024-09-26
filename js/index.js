618b5b130c339706dedc8be4deefa4159b327996
IsaiasTolP <toledop.isaias@gmail.com>
2024-09-26 19:25:28 +0100

    Recogemos pokemons

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-import Pokemon from "./pokemon";
+import Pokemon from "./pokemon.js";
+
+var pokemons = [];
 
 const button = document.querySelector("button");
 
@@ -14,7 +16,7 @@ button.addEventListener("click", () => {
 
     document.querySelector(".listaPokemon").style.visibility = "visible";
 
-    mostrarPokemon();
+    startPokemon();
 });
 
 function mostrarPokemon() {
@@ -30,20 +32,45 @@ const startPokemon = async () => {
     for(var i = 1; i <= 151; i++){
         try{
             await fetch("https://pokeapi.co/api/v2/pokemon/" + i + "/")
-            .then(function result(){
+            .then(function(result){
                 return result.json();
-            })
-            .then(function(data){
+            }).then(function(data){
                 const pokemon = new Pokemon(data);
                 pushPokemon(pokemon);
             });
         }
         catch (error){
-            console.error(`There is an error ${error}`);
+            alert(`There is an error ${error}`);
         }
     }
+
+    await showPokedex();
 }
 
 function pushPokemon(pokemon){
-    pokemon.push(pokemon);
+    pokemons.push(pokemon);
+}
+
+const showPokedex = async () =>{
+    const pokedex = document.getElementById("pokedex");
+    for(var i = 0; i < pokemons.length; i++) {
+        var aux =  0;
+        while (aux != pokemons[i].pkm_type.length) {
+            if (aux == 0)
+                var tipo1 = pokemons[i].pkm_type[aux].type.name;                       
+            if (aux == 1)   
+                var tipo2 = pokemons[i].pkm_type[aux].type.name;
+            else 
+                tipo2 = "";          
+            aux++; 
+        }
+        pokedex.innerHTML +=    `<div class="card">
+                                    <img src="${pokemons[i].pkm_back}">
+                                    <img class="front" src="${pokemons[i].pkm_front}"><br>
+                                    ${pokemons[i].id}. ${pokemons[i].name[0].toUpperCase() + pokemons[i].name.substring(1)}<br>
+                                    <div class="types">
+                                        ${tipo1} ${tipo2}
+                                    </div>
+                                </div>`
+    }
 }
