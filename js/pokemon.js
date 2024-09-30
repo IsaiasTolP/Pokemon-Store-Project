de4a96d0e8a6200523274bf290ebde6c32810417
IsaiasTolP <toledop.isaias@gmail.com>
2024-09-30 14:41:19 +0100

    Pokemon

--- js/pokemon.js
+++
@@ -1,9 +0,0 @@
-export default class Pokemon {
-    constructor(data) {
-        this.name = data.name;
-        this.id = data.id;
-        this.pkm_front = data.sprites.front_default;
-        this.pkm_back = data.sprites.back_default;
-        this.pkm_type = data.types;
-    }
-}
